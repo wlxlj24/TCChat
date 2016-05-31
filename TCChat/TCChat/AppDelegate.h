a32f7cfc194669280c4422b70b6da8e0b18c3c74
TailC <wlxlj24@qq.com>
2016-05-31 13:15:39 +0800

    初始化项目

---
+++ TCChat/TCChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TCChat
+//
+//  Created by TailC on 16/5/31.
+//  Copyright © 2016年 TailC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TCChat/TCChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TCChat
+//
+//  Created by TailC on 16/5/31.
+//  Copyright © 2016年 TailC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
