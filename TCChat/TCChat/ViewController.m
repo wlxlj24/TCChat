9f7c9dcf79f5832c63285e49b781995af785757f
TailC <wlxlj24@qq.com>
2016-05-31 13:48:21 +0800

    pods导入

--- TCChat/TCChat/ViewController.m
+++ TCChat/TCChat/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <EMSDK.h>
 
 @interface ViewController ()
 
@@ -16,12 +17,10 @@
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
 	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
 }
 
 @end
